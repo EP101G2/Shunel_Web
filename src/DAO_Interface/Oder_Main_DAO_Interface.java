675fdfe6747719999652ab016a7b6ebeaa7bc0c7
xuzixiang <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-07-06 22:27:17 +0800

    建立介面以及實作 1.購物車 2.訂單總表 3.訂單明細

--- src/DAO_Interface/Oder_Main_DAO_Interface.java
+++ src/DAO_Interface/Oder_Main_DAO_Interface.java
@@ -5,21 +5,25 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import Bean.Order_Detail;
-import DAO.Order_Detail_DAO;
+import DAO.Oder_Main_DAO;
 import Servlet_Shunel.ServiceLocator;
 
-public class Order_Detail_DAO_Interface implements Order_Detail_DAO{
 
+public class Oder_Main_DAO_Interface implements Oder_Main_DAO{
+	
 	DataSource dataSource;
 
-	public Order_Detail_DAO_Interface() {
+	public Oder_Main_DAO_Interface() {
 		dataSource = ServiceLocator.getInstance().getDataSource();
-
 	}
+	
 
 	@Override
 	public int insert(Order_Detail order_Detail) {
 		// TODO Auto-generated method stub
+
+		
+		
 		return 0;
 	}
 
@@ -36,12 +40,6 @@ public class Order_Detail_DAO_Interface implements Order_Detail_DAO{
 	}
 
 	@Override
-	public byte[] getImage(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public Order_Detail findById(int Order_ID, int Product_ID) {
 		// TODO Auto-generated method stub
 		return null;
@@ -52,4 +50,5 @@ public class Order_Detail_DAO_Interface implements Order_Detail_DAO{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 }
