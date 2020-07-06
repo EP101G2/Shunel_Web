e2ebf5c5bfd9669bde3d86ae3989f08dd722f154
xuzixiang <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-07-07 02:17:20 +0800

    實作Order_Detail_DAO方法

--- src/DAO/Order_Detail_DAO.java
+++ src/DAO/Order_Detail_DAO.java
@@ -3,12 +3,14 @@ package DAO;
 import java.util.List;
 
 import Bean.Order_Detail;
+import Bean.Order_Main;
+import Bean.Product;
 
 public interface Order_Detail_DAO {
 
-	int insert(Order_Detail order_Detail);
+	int insert(Order_Detail order_Detail,Product product);
 
-	int update(Order_Detail order_Detail);
+	int update(Order_Detail order_Detail,Product product);
 
 	int delete(int Order_ID, int Product_ID);
 
--- src/DAO_Interface/Order_Detail_DAO_Interface.java
+++ src/DAO_Interface/Order_Detail_DAO_Interface.java
@@ -1,14 +1,18 @@
 package DAO_Interface;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import Bean.Order_Detail;
+import Bean.Product;
 import DAO.Order_Detail_DAO;
 import Servlet_Shunel.ServiceLocator;
 
-public class Order_Detail_DAO_Interface implements Order_Detail_DAO{
+public class Order_Detail_DAO_Interface implements Order_Detail_DAO {
 
 	DataSource dataSource;
 
@@ -18,13 +22,28 @@ public class Order_Detail_DAO_Interface implements Order_Detail_DAO{
 	}
 
 	@Override
-	public int insert(Order_Detail order_Detail) {
+	public int insert(Order_Detail order_Detail, Product product) {
 		// TODO Auto-generated method stub
-		return 0;
+
+		int count = 0;
+		String sql = "INSERT INTO ORDER_DETAIL" + "(ORDER_ID, PRODUCT_ID, AMOUNT, BUY_PRICE) " + "VALUES(?, ?, ?, ?);";
+		try (Connection connection = dataSource.getConnection();
+				PreparedStatement ps = connection.prepareStatement(sql);) {
+			ps.setInt(1, order_Detail.getOrder_Detail_ID());
+			ps.setInt(2, product.getProduct_ID());
+			ps.setInt(3, order_Detail.getOrder_Detail_Amount());
+			ps.setInt(4, order_Detail.getOrder_Detail_Buy_Price());
+
+			count = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
+
 	}
 
 	@Override
-	public int update(Order_Detail order_Detail) {
+	public int update(Order_Detail order_Detail, Product product) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -32,7 +51,17 @@ public class Order_Detail_DAO_Interface implements Order_Detail_DAO{
 	@Override
 	public int delete(int Order_ID, int Product_ID) {
 		// TODO Auto-generated method stub
-		return 0;
+		int count = 0;
+		String sql = "DELETE FROM ORDER_DETAIL WHERE (ORDER_ID = ?) and (PRODUCT_ID = ?;";
+		try (Connection connection = dataSource.getConnection();
+				PreparedStatement ps = connection.prepareStatement(sql);) {
+			ps.setInt(1, Order_ID);
+			ps.setInt(2, Product_ID);
+			count = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
 	}
 
 	@Override
