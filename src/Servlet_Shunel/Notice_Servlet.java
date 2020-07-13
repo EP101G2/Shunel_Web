71437538c7ad2fbcd811e41466915c0928db8a2d
lin3253job <linlin3253job@gmail.com>
2020-07-13 17:05:24 +0800

    notce server端編寫

--- src/Servlet_Shunel/Notice_Servlet.java
+++ src/Servlet_Shunel/Notice_Servlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
 import Bean.Notice;
@@ -48,48 +49,56 @@ public class Notice_Servlet extends HttpServlet {
 		if (notice_DAO == null) {
 			notice_DAO = new Notice_DAO_Interface();
 		}
+
 		List<Notice> notices = notice_DAO.getNoticeAll();
 		writeText(response, new Gson().toJson(notices));
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		Gson gson = new Gson();
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 		BufferedReader br = request.getReader();
 		StringBuilder jsonIn = new StringBuilder();
 		String line = null;
-		while ((line = br.readLine())!=null) {
+		while ((line = br.readLine()) != null) {
 			jsonIn.append(line);
 		}
 		System.out.print("input:" + jsonIn);
-		
+
 		JsonObject jsonObject = gson.fromJson(jsonIn.toString(), JsonObject.class);
 		if (notice_DAO == null) {
 			notice_DAO = new Notice_DAO_Interface();
 		}
-		
+
 		String action = jsonObject.get("action").getAsString();
-		
+
 		if (action.equals("getNoticeAll")) {
 			List<Notice> notices = notice_DAO.getNoticeAll();
-			writeText(response,gson.toJson(notices));
-		}else if (action.equals("delete")) {
-			
-			
+			writeText(response, gson.toJson(notices));
+		} else if (action.equals("getSaleAll")) {
+			List<Notice> saleNotices = notice_DAO.getSaleAll();
+			writeText(response, gson.toJson(saleNotices));
+		}else if (action.equals("getQAAll")) {
+			List<Notice> qANotices = notice_DAO.getQAAll();
+			writeText(response, gson.toJson(qANotices));
+		}else if (action.equals("getSystemAll")) {
+			List<Notice> systemNotices = notice_DAO.getSystemAll();
+			writeText(response, gson.toJson(systemNotices));
 		}
-		
 	}
 
-	private void writeText(HttpServletResponse response,String outText)throws IOException{
+	private void writeText(HttpServletResponse response, String outText) throws IOException {
 		response.setContentType(CONTENT_TYPE);
 		PrintWriter out = response.getWriter();
 		out.print(outText);
 		// 將輸出資料列印出來除錯用
-		System.out.println("output: " + outText); 
-	 }
+		System.out.println("output: " + outText);
+	}
 
 }
