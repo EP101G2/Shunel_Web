c7fe6119a221d838eeeff0f61ba33ade6aa49f1d
xuzixiang <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-07-22 00:14:59 +0800

    no message

---
+++ src/DAO/Chat_Record_DAO.java
@@ -0,0 +1,28 @@
+package DAO;
+
+import java.util.List;
+
+import Bean.Chat_Record;
+import Bean.Chat_Room;
+
+public interface Chat_Record_DAO {
+
+	int createRoom(String chef_no, String user_no, String user_name, String chef_name);
+
+	long insert(Chat_Record cRecord, byte[] image);
+
+	List<Chat_Record> getAll(int chatRoom);
+
+	int selectId();
+
+	byte[] getImage(int id);
+	
+	int update(String sender, int chatRoom);
+	
+	int selectChatRoom(String chef_no, String user_no);
+	
+	List<Chat_Room> selectChatRoomList(String chef_no, String user_no);
+	
+	
+
+}
