50d231e84c353383f9fee4fb6330c6e0fea3b155
limitis0 <tw85lee@gmail.com>
2020-07-27 13:16:41 +0800

    minor modification

--- src/Servlet_Shunel/Orders_Servlet.java
+++ src/Servlet_Shunel/Orders_Servlet.java
@@ -194,8 +194,13 @@ public class Orders_Servlet extends HttpServlet {
 			writeText(response, gson.toJson(orderShortMainMainList));
 			break;
 		}
+//		get short order detail list insert here
+		case "getOrderDetailShort": {
+			String order_ID = jsonObject.get("Order_ID").getAsString(); //check if "Order_ID" matches the "Order_ID" in client
+//			create method getShortOrderDetails(order_ID)in DAO, DAO Interface
+		}//need to be modify later!!
 		
-//		select by status
+//		select/show by status
 		case "status":{
 			int status = jsonObject.get("status").getAsInt();
 			if (status == 0) {
